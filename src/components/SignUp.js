6abb0b406c67c5022be0c4c52d44d15c3878e574
Edetp27 <Edetp27@gmail.com>
2020-11-27 09:30:42 +0100

    master

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,7 +1,7 @@
 import React, {useRef, useState} from 'react'
-import {Form,Button,Card,Alert} from "react-bootstrap"
-import {useAuth} from "../contexts/AuthContext"
-import {Link} from "react-router-dom"
+import {Form,Button,Card,Alert} from 'react-bootstrap'
+import {useAuth} from '../contexts/AuthContext'
+import {Link,useHistory} from 'react-router-dom'
 
 export default function SignUp() {
     const emailRef = useRef()
@@ -10,6 +10,10 @@ export default function SignUp() {
     const {signup} = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
+    const history = useHistory()
+
+
+
     async function handleSubmit(e){
         e.preventDefault()
         if(passwordRef.current.value !== passwordConfirmRef.current.value){
@@ -19,6 +23,7 @@ export default function SignUp() {
             setError('')
             setLoading(true)
            await signup(emailRef.current.value, passwordRef.current.value)
+           history.push('/')
         }catch{
             setError('failed to create an account')
         }
